b4034b97345a9990eebca3dcc5cfe8a88e1e0d90
Xinhe Ren <jim.x.ren@berkeley.edu>
2018-02-06 22:20:34 -0800

    Revamped streaming part of pipeline

--- src/tcp/configs/alberta_config.py
+++ src/tcp/configs/alberta_config.py
@@ -10,5 +10,12 @@ class Config(object):
 
 
 	def __init__(self):
+		# Stream Capture
+		self.STREAM_OUTPUT_SEGMENT_TIME_LIMIT = 60 	# approximate length of video segments in seconds
+		self.STREAM_OUTPUT_DIR_SIZE_LIMIT = 1e9 	# maximum size limit for downloaded video in bytes
+
+		# Object Detection
 		self.check_point_path = 'Checkpoints/VGG_VOC0712_SSD_300x300_ft_iter_120000.ckpt'
+
+		# Registration
 
