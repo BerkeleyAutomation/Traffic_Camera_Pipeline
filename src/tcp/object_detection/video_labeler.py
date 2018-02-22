76218e6e09546fbba1e8a1f509352ed57148a457
Xinhe Ren <jim.x.ren@berkeley.edu>
2018-02-21 19:24:39 -0800

    Added pickling to video labler

--- src/tcp/object_detection/video_labeler.py
+++ src/tcp/object_detection/video_labeler.py
@@ -5,6 +5,7 @@ import sys, os
 import cv2
 import youtube_dl
 import numpy as np
+import cPickle as pickle
 from urlparse import parse_qs
 
 from AbstractDetector import AbstractDetector
@@ -14,6 +15,7 @@ from tcp.object_detection.cropper import Cropper
 import IPython
 
 from tcp.object_detection.init_labeler import InitLabeler
+from tcp.object_detection.init_labeler_opencv import InitLabeler_OpenCV
 
 
 class LabelVideo():
@@ -26,17 +28,39 @@ class LabelVideo():
         self.ssd_detector = SSD_VGG16Detector('ssd_vgg16', self.config.check_point_path, cropper=self.cropper)
         self.t = 0
 
-    def label_video(self, video_path, output_limit=500, num_skip_frames=1):
-            self.ssd_detector.setStreamURL(video_path)
-            if not AbstractDetector.openCapture(self.ssd_detector):
-                raise ValueError('Video file %s failed to open.' % video_path)
-            print 'Scanning %s...' % (video_path)
+    def label_video(self, video_path, output_limit=500, num_skip_frames=1, debug_pickle=False):
+        video_name = os.path.splitext(os.path.basename(video_path))[0]
+
+        self.ssd_detector.setStreamURL(video_path)
+        if not AbstractDetector.openCapture(self.ssd_detector):
+            raise ValueError('Video file %s failed to open.' % video_path)
+        print 'Scanning %s...' % (video_path)
 
 
-            ### First pass: get bounding ###
-            all_rclasses = []
-            all_rscores = []
-            all_rbboxes = []
+        ### First pass: get bounding ###
+        all_rclasses = []
+        all_rscores = []
+        all_rbboxes = []
+
+        if debug_pickle:
+            try:
+                all_rclasses = pickle.load(open('Debug_Pickles/%s_classes.cpkl' % video_name, 'r'))
+                print 'Loaded "Debug_Pickles/%s_classes.cpkl"' % video_name
+            except IOError as e:
+                print 'Unable to load "Debug_Pickles/%s_classes.cpkl"' % video_name
+            try:
+                all_rscores = pickle.load(open('Debug_Pickles/%s_scores.cpkl' % video_name, 'r'))
+                print 'Loaded "Debug_Pickles/%s_scores.cpkl"' % video_name
+            except IOError as e:
+                print 'Unable to load "Debug_Pickles/%s_scores.cpkl"' % video_name
+            try:
+                all_rbboxes = pickle.load(open('Debug_Pickles/%s_bboxes.cpkl' % video_name, 'r'))
+                print 'Loaded "Debug_Pickles/%s_bboxes.cpkl"' % video_name
+            except IOError as e:
+                print 'Unable to load "Debug_Pickles/%s_bboxes.cpkl"' % video_name
+
+        if all_rclasses == [] or all_rscores == [] or all_rbboxes == []:
+            print 'Some detection pickle file failed to load. Running detector network...'
             while self.ssd_detector.cap.isOpened():
                 ret, frame = self.ssd_detector.cap.read()
                 if frame is None:
@@ -51,67 +75,75 @@ class LabelVideo():
                 all_rbboxes.append(rbboxes)
             self.ssd_detector.cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
-            ### CALL INITIAL LABELER ###
-            self.init_labeler = InitLabeler(self.config, self.ssd_detector.cap, all_rbboxes, all_rclasses)
+        if debug_pickle:
+            if not os.path.exists('Debug_Pickles/'):
+                os.makedirs('Debug_Pickles/')
+            pickle.dump(all_rclasses, open('Debug_Pickles/%s_classes.cpkl' % video_name, 'w+'))
+            pickle.dump(all_rscores, open('Debug_Pickles/%s_scores.cpkl' % video_name, 'w+'))
+            pickle.dump(all_rbboxes, open('Debug_Pickles/%s_bboxes.cpkl' % video_name, 'w+'))
 
-            output_count = 0
-            frame_skip = 0
+        ### CALL INITIAL LABELER ###
+        self.init_labeler = InitLabeler(self.config, self.ssd_detector.cap, all_rbboxes, all_rclasses,
+                                        init_labeler_pickle_path='Debug_Pickles/%s_init_labels.cpkl' % video_name,
+                                        cache_frames=False)
 
-            trajectory = []
+        output_count = 0
+        frame_skip = 0
 
-
-            ### Second pass: process bounding box data ###
-            while self.ssd_detector.cap.isOpened():
-                ret, frame = self.ssd_detector.cap.read()
-                self.t += 1
-                if frame is None:
-                    break
-
-                frame_skip += 1
-                output_count += 1
-                assert num_skip_frames > 0 and int(num_skip_frames) == num_skip_frames
-                frame_skip %= num_skip_frames
-                if frame_skip != 0:
-                    continue
-
-                # Process frame here
-                rclasses = all_rclasses[output_count]
-                rscores = all_rscores[output_count]
-                rbboxes = all_rbboxes[output_count]
-
-                # ###CALL INITIAL LABELER###
-                # self.init_labeler(img)
-
-                if self.config.save_images:
-                    
-                    cv2.imwrite(self.config.save_debug_img_path+'img_'+str(output_count)+'.png',img)
-                
-                unique, counts = np.unique(rclasses, return_counts=True)
-                classes_counts = dict(zip(unique, counts))
-                car_count = classes_counts.get(7)
-                print "T ", self.t
-                current_frame = []
-
-                car_cords = self.get_car_cords(output_count, rclasses, rbboxes)
-                ped_cords = self.get_pedestrian_cords(rclasses, rbboxes)
-
-                if output_count > output_limit:
-                    break
-
-                for car_cord in car_cords:
-                    current_frame.append(car_cord)
-
-                if self.config.use_pedestrian:
-                    for ped_cord in ped_cords:
-                        current_frame.append(ped_cord)  
-
-                if not len(current_frame) == 0:
-                    trajectory.append(current_frame)
+        trajectory = []
 
 
-            self.ssd_detector.cap.release()
+        ### Second pass: process bounding box data ###
+        while self.ssd_detector.cap.isOpened():
+            ret, frame = self.ssd_detector.cap.read()
+            self.t += 1
+            if frame is None:
+                break
+
+            frame_skip += 1
+            output_count += 1
+            assert num_skip_frames > 0 and int(num_skip_frames) == num_skip_frames
+            frame_skip %= num_skip_frames
+            if frame_skip != 0:
+                continue
+
+            # Process frame here
+            rclasses = all_rclasses[output_count]
+            rscores = all_rscores[output_count]
+            rbboxes = all_rbboxes[output_count]
+
+            # ###CALL INITIAL LABELER###
+            # self.init_labeler(img)
+
+            if self.config.save_images:
+                cv2.imwrite(self.config.save_debug_img_path+'img_'+str(output_count)+'.png',img)
             
-            return trajectory
+            unique, counts = np.unique(rclasses, return_counts=True)
+            classes_counts = dict(zip(unique, counts))
+            car_count = classes_counts.get(7)
+            print "T ", self.t
+            current_frame = []
+
+            car_cords = self.get_car_cords(output_count, rclasses, rbboxes)
+            ped_cords = self.get_pedestrian_cords(rclasses, rbboxes)
+
+            if output_count > output_limit:
+                break
+
+            for car_cord in car_cords:
+                current_frame.append(car_cord)
+
+            if self.config.use_pedestrian:
+                for ped_cord in ped_cords:
+                    current_frame.append(ped_cord)  
+
+            if not len(current_frame) == 0:
+                trajectory.append(current_frame)
+
+
+        self.ssd_detector.cap.release()
+        
+        return trajectory
 
 
     def get_car_cords(self, frame_i, rclasses, rbboxes):
